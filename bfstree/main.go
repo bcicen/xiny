4c76a9f95653d1c39cf9fd44bfdbd9a88a0ccea4
Bradley Cicenas <bradley@vektor.nyc>
2018-01-02 13:33:19 +0000

    extend bfstree helper methods, tests

--- bfstree/main.go
+++ bfstree/main.go
@@ -2,53 +2,12 @@ package bfstree
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Edge interface {
 	From() string
 	To() string
-}
-
-type Path struct {
-	edges []Edge
-}
-
-func NewPath(edges ...Edge) *Path { return &Path{edges} }
-
-func (p *Path) Edges() []Edge { return p.edges }
-func (p *Path) String() string {
-	s := fmt.Sprintf("%s->%s", p.edges[0].From(), p.edges[0].To())
-	if len(p.edges) > 1 {
-		for _, e := range p.edges[1:] {
-			s += fmt.Sprintf("->%s", e.To())
-		}
-	}
-	return s
-}
-
-func (p *Path) Append(e Edge) { p.edges = append(p.edges, e) }
-func (p *Path) Last() Edge    { return p.edges[len(p.edges)-1] }
-
-// Return whether a given edge, if added, would result in
-// a circular or recursive path
-func (p *Path) IsCircular(edge Edge) bool {
-	child := edge.To()
-	for _, e := range p.edges {
-		if e.From() == child || e.To() == child {
-			return true
-		}
-	}
-	return false
-}
-
-// return whether this path transverses a given node name
-func (p *Path) HasNode(s string) bool {
-	for _, e := range p.edges {
-		if e.From() == s || e.To() == s {
-			return true
-		}
-	}
-	return false
 }
 
 type BFSTree struct {
@@ -57,24 +16,22 @@ type BFSTree struct {
 
 func NewBFSTree(edges ...Edge) *BFSTree { return &BFSTree{edges} }
 
+func (b *BFSTree) Len() int       { return len(b.edges) }
+func (b *BFSTree) Edges() []Edge  { return b.edges }
 func (b *BFSTree) AddEdge(e Edge) { b.edges = append(b.edges, e) }
 
 // Return unique node names
-func (b *BFSTree) Nodes() (names []string) {
-	nmap := make(map[string]int)
+func (b *BFSTree) Nodes() []string {
+	var names []string
 	for _, e := range b.edges {
-		nmap[e.To()] = 0
-		nmap[e.From()] = 0
+		names = append(names, e.To())
+		names = append(names, e.From())
 	}
-
-	for n, _ := range nmap {
-		names = append(names, n)
-	}
-	return names
+	return uniq(names)
 }
 
 // return edges from a given start point
-func (b *BFSTree) fromEdges(start string) (res []Edge) {
+func (b *BFSTree) fromNode(start string) (res []Edge) {
 	for _, e := range b.edges {
 		if e.From() == start {
 			res = append(res, e)
@@ -86,8 +43,8 @@ func (b *BFSTree) fromEdges(start string) (res []Edge) {
 func (b *BFSTree) FindPath(start string, end string) (path *Path, err error) {
 	var paths []*Path
 
-	// create start nodes
-	for _, e := range b.fromEdges(start) {
+	// Create start paths from origin node
+	for _, e := range b.fromNode(start) {
 		p := NewPath(e)
 		if e.To() == end {
 			return p, nil
@@ -95,15 +52,13 @@ func (b *BFSTree) FindPath(start string, end string) (path *Path, err error) {
 		paths = append(paths, p)
 	}
 
-	for {
+	for len(paths) > 0 {
 		var newPaths []*Path
 
-		if len(paths) == 0 {
-			break
-		}
+		fmt.Printf("nopaths %d\n", len(paths))
 
 		for _, p := range paths {
-			children := b.fromEdges(p.Last().To())
+			children := b.fromNode(p.Last().To())
 
 			// maximum path depth reached, drop
 			if len(children) == 0 {
@@ -118,7 +73,7 @@ func (b *BFSTree) FindPath(start string, end string) (path *Path, err error) {
 				}
 
 				np := NewPath(p.edges...)
-				np.Append(e)
+				np.AddEdge(e)
 
 				if e.To() == end {
 					return np, nil
@@ -130,4 +85,60 @@ func (b *BFSTree) FindPath(start string, end string) (path *Path, err error) {
 	}
 
 	return path, fmt.Errorf("no path found")
+}
+
+type Path struct {
+	*BFSTree
+}
+
+func NewPath(edges ...Edge) *Path { return &Path{&BFSTree{edges}} }
+
+func (p *Path) Last() Edge { return p.edges[len(p.edges)-1] }
+
+// Returns names for all path nodes in the order they are transversed
+func (p *Path) Nodes() []string {
+	names := []string{p.edges[0].From()}
+	for _, e := range p.edges {
+		names = append(names, e.To())
+	}
+	return names
+}
+
+func (p *Path) String() string { return strings.Join(p.Nodes(), "->") }
+
+// Return whether a given edge, if added, would result in
+// a circular or recursive path
+func (p *Path) IsCircular(edge Edge) bool {
+	child := edge.To()
+	for _, e := range p.edges {
+		if e.From() == child || e.To() == child {
+			return true
+		}
+	}
+	return false
+}
+
+// Return whether this path transverses a given node name
+func (p *Path) HasNode(s string) bool {
+	for _, e := range p.edges {
+		if e.From() == s || e.To() == s {
+			return true
+		}
+	}
+	return false
+}
+
+// uniq returns a unique subset of the string slice provided.
+func uniq(a []string) []string {
+	u := make([]string, 0, len(a))
+	m := make(map[string]bool)
+
+	for _, val := range a {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+
+	return u
 }
--- bfstree/main_test.go
+++ bfstree/main_test.go
@@ -1,8 +1,10 @@
 package bfstree
 
-import "testing"
+import (
+	"testing"
+)
 
-var tree BFSTree
+var tree *BFSTree
 
 // TestEdge is an Edge interface implementation
 type TestEdge struct {
@@ -25,14 +27,29 @@ func TestCreateTree(t *testing.T) {
 		TestEdge{"a", "c"},
 		TestEdge{"b", "c"},
 	)
-	t.Logf("created tree with %d edges, %d nodes", len(tree), len(tree.Nodes()))
+	t.Logf("created tree with %d edges, %d nodes", tree.Len(), len(tree.Nodes()))
 }
 
-func TestFindPath(t *testing.T) {
-
+func TestFindLongPath(t *testing.T) {
 	path, err := tree.FindPath("a", "g")
 	if err != nil {
 		t.Error(err)
 	}
 	t.Logf("found path: %s", path)
+}
+
+func TestFindShortPath(t *testing.T) {
+	path, err := tree.FindPath("a", "b")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("found path: %s", path)
+}
+
+func TestFindNoPath(t *testing.T) {
+	_, err := tree.FindPath("a", "z")
+	if err == nil {
+		t.Errorf("no error returned on missing path")
+	}
+	t.Logf("got expected error: %s", err)
 }
