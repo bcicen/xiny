82475c0cca3141912836a39b180a3827a70d58b6
Bradley Cicenas <bradley@vektor.nyc>
2018-01-04 11:04:25 +0000

    move cmdline parse to own file

--- main.go
+++ main.go
@@ -3,27 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
 	"strings"
 
 	"github.com/bcicen/xiny/log"
 	"github.com/bcicen/xiny/units"
 )
-
-var re = regexp.MustCompile("(-?[0-9.]+)\\s*(\\w+)\\s+in\\s+(\\w+)")
-
-func parse(s string) (float64, string, string) {
-	mg := re.FindStringSubmatch(s)
-	if len(mg) != 4 {
-		panic(fmt.Errorf("parse error"))
-	}
-	q, err := strconv.ParseFloat(mg[1], 6)
-	if err != nil {
-		panic(fmt.Errorf("parse error"))
-	}
-	return q, mg[2], mg[3]
-}
 
 func handleOpt(opt string) {
 	switch opt {
@@ -37,20 +21,6 @@ func handleOpt(opt string) {
 	default:
 		exitErr(fmt.Errorf("unknown option: -%s", opt))
 	}
-}
-
-// parse out and handle options, returning the remainder of the command line
-func parseOpts(s string) string {
-	ore := regexp.MustCompile("-([a-z]+)")
-	matches := ore.FindAllString(s, -1)
-
-	for _, m := range matches {
-		opt := strings.TrimPrefix(m, "-")
-		handleOpt(opt)
-		s = strings.Replace(s, m, "", 1)
-	}
-
-	return s
 }
 
 func usage() {
@@ -71,9 +41,12 @@ func main() {
 		usage()
 	}
 
-	cmd := parseOpts(strings.Join(os.Args[1:], " "))
+	cmd := strings.Join(os.Args[1:], " ")
+	for _, opt := range parseOpts(&cmd) {
+		handleOpt(opt)
+	}
 
-	q, u1, u2 := parse(cmd)
+	q, u1, u2 := parseCmd(cmd)
 
 	fromUnit, err := units.Find(u1)
 	if err != nil {
---
+++ parse.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	cmdRe = regexp.MustCompile("(-?[0-9.]+)\\s*(\\w+)\\s+in\\s+(\\w+)")
+	optRe = regexp.MustCompile("-([a-z]+)")
+)
+
+func parseCmd(s string) (float64, string, string) {
+	mg := cmdRe.FindStringSubmatch(s)
+	if len(mg) != 4 {
+		panic(fmt.Errorf("parse error"))
+	}
+	q, err := strconv.ParseFloat(mg[1], 6)
+	if err != nil {
+		panic(fmt.Errorf("parse error"))
+	}
+	return q, mg[2], mg[3]
+}
+
+// parse out options
+func parseOpts(s *string) (cmd string, opts []string) {
+	ns := *s
+
+	matches := optRe.FindAllString(ns, -1)
+	for _, m := range matches {
+		opts = append(opts, strings.TrimPrefix(m, "-"))
+		ns = strings.Replace(ns, m, "", 1)
+	}
+
+	s = &ns
+	return opts
+}
