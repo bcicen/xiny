2d423efd99c3caaff1b0b0f6bf4b52bf23fa3522
Bradley Cicenas <bradley@vektor.nyc>
2018-01-20 16:49:42 +0000

    move conversion to own package file

---
+++ units/conversion.go
@@ -0,0 +1,58 @@
+package units
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	valuate "github.com/Knetic/govaluate"
+)
+
+type ConversionFn func(float64) float64
+
+type Conversion struct {
+	from    Unit
+	to      Unit
+	Fn      ConversionFn
+	Formula string
+}
+
+// Conversion implements bfstree.Edge interface
+func (c Conversion) To() string   { return c.to.Name }
+func (c Conversion) From() string { return c.from.Name }
+
+func (c Conversion) String() string { return c.Formula }
+
+func newConversion(from, to Unit, formula string) Conversion {
+	expr, err := valuate.NewEvaluableExpression(formula)
+	if err != nil {
+		panic(err)
+	}
+
+	// create conversion function
+	fn := func(x float64) float64 {
+		params := make(map[string]interface{})
+		params["x"] = x
+
+		res, err := expr.Evaluate(params)
+		if err != nil {
+			panic(err)
+		}
+		return res.(float64)
+	}
+
+	return Conversion{from, to, fn, fmtFormula(formula)}
+}
+
+func fmtFormula(s string) string {
+	re := regexp.MustCompile("(-?[0-9.]+)")
+	for _, match := range re.FindAllString(s, -1) {
+		f, err := strconv.ParseFloat(match, 64)
+		if err != nil {
+			panic(err)
+		}
+		s = strings.Replace(s, match, fmt.Sprintf("%g", f), 1)
+	}
+	return s
+}
--- units/quantity.go
+++ units/quantity.go
@@ -2,11 +2,8 @@ package units
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
 	"strings"
 
-	valuate "github.com/Knetic/govaluate"
 	"github.com/bcicen/bfstree"
 	"github.com/bcicen/xiny/log"
 )
@@ -14,21 +11,6 @@ import (
 var (
 	QuantityMap = make(map[string]*Quantity)
 )
-
-type ConversionFn func(float64) float64
-
-type Conversion struct {
-	from    Unit
-	to      Unit
-	Fn      ConversionFn
-	Formula string
-}
-
-// Conversion implements bfstree.Edge interface
-func (c Conversion) To() string   { return c.to.Name }
-func (c Conversion) From() string { return c.from.Name }
-
-func (c Conversion) String() string { return c.Formula }
 
 type Quantity struct {
 	Name string
@@ -55,27 +37,11 @@ func (q *Quantity) NewRatioConv(from, to Unit, ratio float64) {
 	q.NewConv(to, from, fmt.Sprintf("x / %s", ratioStr))
 }
 
-// Create a new conversion from one unit to another
+// Create a new conversion from one unit to another, within this quantity
 func (q *Quantity) NewConv(from, to Unit, formula string) {
-	expr, err := valuate.NewEvaluableExpression(formula)
-	if err != nil {
-		panic(err)
-	}
-
-	// create conversion function
-	fn := func(x float64) float64 {
-		params := make(map[string]interface{})
-		params["x"] = x
-
-		res, err := expr.Evaluate(params)
-		if err != nil {
-			panic(err)
-		}
-		return res.(float64)
-	}
-
+	conv := newConversion(from, to, formula)
 	log.Debugf("loaded conversion %s -> %s", from.Name, to.Name)
-	q.conv = append(q.conv, Conversion{from, to, fn, fmtFormula(formula)})
+	q.conv = append(q.conv, conv)
 }
 
 // Resolve a path of one or more conversions between two units
@@ -117,16 +83,4 @@ func (q *Quantity) lookup(from, to string) (c Conversion, err error) {
 		}
 	}
 	return c, fmt.Errorf("conversion not found")
-}
-
-func fmtFormula(s string) string {
-	re := regexp.MustCompile("(-?[0-9.]+)")
-	for _, match := range re.FindAllString(s, -1) {
-		f, err := strconv.ParseFloat(match, 64)
-		if err != nil {
-			panic(err)
-		}
-		s = strings.Replace(s, match, fmt.Sprintf("%g", f), 1)
-	}
-	return s
 }
